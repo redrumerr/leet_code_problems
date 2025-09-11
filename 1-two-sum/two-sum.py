c60935abbfca8e9a82715394ac3d8800755c5052
redrumerr <120748753+redrumerr@users.noreply.github.com>
2025-09-11 11:22:27 +0300

    Time: 893 ms (43.95%) | Memory: 13.3 MB (31.08%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) == 2 and sum(nums) == target:
+            return [i for i in range(len(nums))]
+
+        hash_map = dict()
+        for i in range(len(nums)):
+            hash_map[nums[i]] = i
+
+        for i in range(len(nums)):
+            x = target - nums[i]
+            if nums.count(x) <= 0:
+                continue
+            else:
+                if x in hash_map and i != hash_map[x]:
+                    return [i, hash_map[x]]
+            
+        
