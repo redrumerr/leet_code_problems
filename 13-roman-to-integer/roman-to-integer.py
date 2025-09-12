8210352d3707a4bf1881090262c0e295cedb49ca
redrumerr <120748753+redrumerr@users.noreply.github.com>
2025-09-12 17:29:40 +0300

    Time: 11 ms (43.18%) | Memory: 12.6 MB (13.14%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -4,21 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if s == 'IV':
-            return 4
-        elif s == 'IX':
-            return 9
         roman_digits = {'I': 1, 'V': 5, 'X': 10,
                         'L': 50, 'C': 100, 'D': 500,
                         'M': 1000}
         roman_num = 0
         for i in range(len(s)):
-            curr = roman_digits[s[i]]
-            next_val = roman_digits[s[i + 1]] if i + 1 < len(s) else 0
-            if curr < next_val:
-                roman_num -= curr
+            if i + 1 < len(s) and roman_digits[s[i]] < roman_digits[s[i + 1]]:
+                roman_num -= roman_digits[s[i]]
             else:
-                roman_num += curr
+                roman_num += roman_digits[s[i]]
         return roman_num
-        
         
